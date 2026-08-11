30a4d41c2e13b94061e7b5c236cf10d6cac33897
vishal prajapati <vishalprajapati935498@gmail.com>
2026-08-11 18:57:52 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -10,19 +10,19 @@ from src.logger import logging
 # --------------------------------------------------------------------------------
 
 # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
